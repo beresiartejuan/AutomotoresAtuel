6476703ccb9574ca5034a1ea8315f7026d228eec
juan beresiarte <juanberesiarte@gmail.com>
2024-10-15 23:39:49 -0300

    Create a mook function to test the product list

---
+++ client/src/mooks/products.ts
@@ -0,0 +1,40 @@
+export function getAllProducts() {
+    return [
+        {
+            id: "1",
+            name: "Toyota Corolla",
+            model: "2024",
+            image: "/auto_1.jpg"
+        },
+        {
+            id: "2",
+            name: "Ford Ranger",
+            model: "2023",
+            image: "/auto_2.jpg"
+        },
+        {
+            id: "3",
+            name: "Chevrolet Onix",
+            model: "2023",
+            image: "/auto_3.jpg"
+        },
+        {
+            id: "4",
+            name: "Volkswagen Amarok",
+            model: "2022",
+            image: "/auto_4.jpg"
+        },
+        {
+            id: "5",
+            name: "Fiat Cronos",
+            model: "2024",
+            image: "/auto_5.jpg"
+        },
+        {
+            id: "6",
+            name: "Peugeot 208",
+            model: "2023",
+            image: "/auto_6.jpg"
+        }
+    ];
+}
